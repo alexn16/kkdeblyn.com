9c98358e990f07801bed7a51505595eece4cb8a8
Alex <alejandro.velasco.monje@gmail.com>
2016-07-04 09:46:58 +0200

    Revert "Revert "fix""
    This reverts commit d808be926912693cd85412ba792aa2246812d93a.

--- site/web/wp-config.php
+++ site/web/wp-config.php
@@ -7,3 +7,11 @@
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 require_once(dirname(__DIR__) . '/config/application.php');
 require_once(ABSPATH . 'wp-settings.php');
+define( 'WP_ALLOW_MULTISITE', true );
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', true );
+$base = '/';
+define( 'DOMAIN_CURRENT_SITE', '${HTTP_HOST}' );
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );/* That's all, stop editing!
