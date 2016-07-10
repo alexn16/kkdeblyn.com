ba510f994efdd70232661f50ffe28d4916dd7d2a
Alex <alejandro.velasco.monje@gmail.com>
2016-07-10 23:42:13 +0200

    fix

--- site/web/app/themes/klarkdeblyn/templates/custom-header.php
+++
@@ -1,7 +0,0 @@
-<head>
-
-  <div id="image-header">
-    <img src="<?= get_template_directory_uri(); ?>/assets/images/robot2.jpg" class="img-responsive" alt="Responsive image" align="middle" >
-  </div>
-
-</head>
